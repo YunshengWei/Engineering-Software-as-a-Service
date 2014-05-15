10ec55dd422774b38291fcf165fbb094efff72bc
YunshengWei <weiyunshengwei@gmail.com>
2014-05-15 08:43:10 -0300

    Finished HW1-4

--- HW1/lib/attr_accessor_with_history.rb
+++ HW1/lib/attr_accessor_with_history.rb
@@ -4,7 +4,12 @@ class Class
     attr_reader attr_name # create the attribute's getter
     attr_reader attr_name+"_history" # create bar_history getter
     class_eval %Q{
-      # YOUR CODE HERE
+      @#{attr_name}_history = nil
+      def #{attr_name}=(value)
+        @#{attr_name}_history ||= []
+        @#{attr_name}_history << @#{attr_name}
+        @#{attr_name} = value
+      end
     }
   end
 end
